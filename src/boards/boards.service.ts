5183e723c719d4ce2a8b0ab3a919228b1588fafb
blu3fishez <kcw2205@naver.com>
2022-01-15 07:13:16 +0000

    #5 게시물 생성하기 -1 service 로직 만들기

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './board.model';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class BoardsService {
@@ -7,5 +8,18 @@ export class BoardsService {
 	
 	getAllBoards(): Board[] {
 		return this.boards;
+	}
+	
+	createBoard(title:string, description:string) {
+		// 생성 로직을 만듬.
+		const board: Board = {
+			id: uuid(),
+			title, // ES6 이후에서는 이런 방식이 지원됨
+			description,
+			status:BoardStatus.PUBLIC
+		}
+		
+		this.boards.push(board);
+		return board;
 	}
 }
