37d20b5df923ad7133d3ec2cadbb5297b3a85560
blu3fishez <kcw2205@naver.com>
2022-01-15 09:18:52 +0000

    #7 Parameter 읽어들이고 라우팅하기 / 게시글 검색하기

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Query } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './board.model'
 import { CreateBoardDto } from './dto/create-board.dto'
@@ -27,5 +27,12 @@ export class BoardsController {
 		// express 에서 reqest의 body는 req.body 라고 bodyParser 로 받아옴
 		// Nestjs에서는 @Body('title') 하면 title에 대한 정보를 받고, @Body () 로 하면 모든 값을 받아올 수 있다.
 		return this.boardsService.createBoard(createBoardDto);
+	}
+	
+	@Get('/') // URL의 와일드셋(파라미터)을 가져오는 방법
+	// 쿼리스트링도 @Param('id') 와 같이 가져오면된다.
+	// @Param() 은 string 배열을 반환한다.
+	getBoardById(@Query('id') id: string): Board{
+		return this.boardsService.getBoardById(id);
 	}
 }
